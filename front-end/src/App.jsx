8dd4da9045c2f97ba934c99d8aeca2ddb598dd8a
Kims-club98 <naxen5611@naver.com>
2025-12-19 13:19:33 +0900

    gitignore 추가 및 README file

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,15 @@
-import './App.css';
+// 리엑트에서는 태그를 만들어 사용
+// 주의: 컴포넌트 이름은 반드시 대문자로
+//화살표 함수로 변경한 부분
+//() => ()
+//() => {}
+
+
 
 const App = () => {
+  // 선언부
   return (
-    <div className="App">
+    <div style ={{height: '100vh'}}">
       Hello React!!!
     </div>
   );
---
+++ front-end/src/components/auth/include/pages/HomePages.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepages = () => {
+  return (
+    <div>
+      HomePage
+    </div>
+  )
+}
+
+export default Homepages
+
