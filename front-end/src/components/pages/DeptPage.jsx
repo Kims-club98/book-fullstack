4cf2ed4e563f6d3779b72591947ccde7a1662a79
Kims-club98 <naxen5611@naver.com>
2025-12-19 16:25:00 +0900

    251219 16:30

--- front-end/src/components/pages/DeptPage.jsx
+++ front-end/src/components/pages/DeptPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import DeptList from '../dept/DeptList' // Import 된 것을 확인 可
+import Header from '../include/Header'
+import Footer from '../include/footer'
 
 const DeptPage = () => {
   return (
     <>
+    <Header/>
       <h2>부서 관리</h2>
       <DeptList/> {/*사용자 정의 태크 html처럼 불러오기 가능. */}
+    <Footer / >
+
     </>
   )
 }
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import BookApp from './BookApp';
 import { BrowserRouter } from 'react-router';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
