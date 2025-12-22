ef96540b82903447476e996f3925f10c08c998c1
Kims-club98 <naxen5611@naver.com>
2025-12-22 16:08:26 +0900

    251222

--- front-end/src/components/auth/LoginForm.jsx
+++ front-end/src/components/auth/LoginForm.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { loginGoogle } from '../service/authLogin'
+import { useNavigate } from 'react-router'
 
 const LoginForm = () => {
+  const navigate = useNavigate()
+  const handleLogin = async () => {
+    try {
+      await loginGoogle()
+      navigate("/")
+    } catch (error) {
+      console.error("로그인 실패", error)
+    }
+  }
   return (
     <>
-    로그인 화면 
+      <h2>로그인화면</h2>
+      <button type='button' onClick={handleLogin}>구글로그인</button>
     </>
   )
 }
-
 export default LoginForm
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,20 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import AuthLogic from './components/service/authLogin';
-import app from './components/service/firebase';
 
-// AuthLogic 객체 생성하기
-const authLogic = new AuthLogic(app);
-console.log(authLogic);
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-  // 각 파일 이동
-  <>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
-
