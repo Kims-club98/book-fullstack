7397d2146ba3f16609f809fd958349d1109bd771
키위 <slalom0914@gmail.com>
2026-01-02 18:03:22 +0900

    8교시-백엔드 수정본

--- back-end/src/main/java/com/example/demo/controller/DeptController.java
+++ back-end/src/main/java/com/example/demo/controller/DeptController.java
@@ -1,78 +1,89 @@
 package com.example.demo.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.example.demo.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+//응답 페이지를 jsp로 가져갈때는 아래를 사용함.
+//import org.springframework.stereotype.Controller;
+//응답 결과를 json, text/plain
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.vo.DeptVO;
 import com.google.gson.Gson;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 import lombok.extern.log4j.Log4j2;
-import com.google.gson.Gson;
+
+/*
+MVC패턴 기준
+컨트롤계층 - 사용자가 입력한 값 청취, 응답페이지에 대한 결과 처리(html, json)
+서블릿과는 달리 서블릿을 상속받지 않고 처리한다. - 대신 어노테이션을 사용함.
+복잡한 환경 설정, 의존성 관리
+http://localhost:8000/dept/deptList
+http://localhost:8000/dept/deptDetail
+http://localhost:8000/dept/deptInsert
+http://localhost:8000/dept/deptUpdate
+http://localhost:8000/dept/deptDelete
+*/
 
 @Log4j2
-public class DeptController extends HttpServlet {
+@RestController
+@RequestMapping("/dept/*")
+public class DeptController {
+	@Autowired
+	private DeptService deptService;// 변수 이름은 줄여쓰지 않음.
 
-	// 부서 정보 삭제하기
-	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		log.info("doDelete");
+	@GetMapping("deptList")
+	public String deptList(DeptVO dvo) {
+		log.info("deptList호출");
+		log.info("dname : " + dvo.getDname());
+		List<Map<String, Object>> list = null;
+		list = deptService.deptList(dvo);
+		String temp = null;
+		Gson gson = new Gson();
+		temp = gson.toJson(list);
+		return temp;// application/json
 	}
 
-	// 부서 정보 조회하기
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 사용자(3000번 서버)가 파라미터로 넘긴 값 출력해 보기
-		String deptno = req.getParameter("deptno");
-		log.info("사용자가 입력한 부서번호 : " + deptno);
-		resp.setContentType("application/json;utf-8");
-		PrintWriter out = resp.getWriter();
-
-		List<Map<String, Object>> list = new ArrayList<>();
-		Map<String, Object> map = new HashMap<>();
-		map.put("deptno", 10);
-		map.put("dname", "총무부");
-		map.put("loc", "서울");
-		list.add(map);
-		map = new HashMap<>();
-		map.put("deptno", 20);
-		map.put("dname", "개발부");
-		map.put("loc", "부산");
-		list.add(map);
-		map = new HashMap<>();
-		map.put("deptno", 30);
-		map.put("dname", "운영부");
-		map.put("loc", "제주");
-		list.add(map);
-
-		String imsi = null;
-		Gson g = new Gson();
-		imsi = g.toJson(list);
-		out.print(imsi);
+	@GetMapping("deptDetail")
+	public String deptDetail() {
+		log.info("deptDetail호출");
+		return "deptDetail";
 	}
 
-	// 부서 정보 등록하기
-	// 사용자로 부터 입력받은 값을 전달 받아서 처리하기
-	// Front-End : React -> JSON형식
-	// jsp : 폼 전송 받음 ,
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		log.info("doPost");
-		String deptno = req.getParameter("deptno");
-		String dname = req.getParameter("dname");
-		String loc = req.getParameter("loc");
-		log.info(deptno + ", " + dname + ", " + loc);
+	@PostMapping("deptInsert")
+	public String deptInsert(@RequestBody Map<String, Object> pmap) {
+		log.info("deptInsert호출");
+		log.info(pmap);
+		int result = -1;
+		result = deptService.deptInsert(pmap);
+		return String.valueOf(result);// 이 반환값은 리액트 페이지로 반환됨
+		// return "입력성공|실패";
 	}
 
-	// 부서 정보 수정하기
-	@Override
-	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		log.info("doPut");
+	@PutMapping("deptUpdate")
+	public String deptUpdate(@RequestBody Map<String, Object> pmap) {
+		log.info("deptUpdate호출");
+		int result = 0;
+		result = deptService.deptUpdate(pmap);
+		return String.valueOf(result);// 1-> "1"
 	}
-
+	// http://localhost:8000/dept/deptDelete?deptno=61
+	// http://localhost:8000/dept/61
+	@DeleteMapping("deptDelete")
+	public String deptDelete(DeptVO dvo) {
+		log.info("deptDelete호출");
+		log.info("사용자가 선택한 부서 번호 : "+dvo);
+		int result = -1;
+		result = deptService.deptDelete(dvo);
+		return String.valueOf(result);
+	}
 }
